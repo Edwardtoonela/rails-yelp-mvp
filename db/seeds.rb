e8c695947b5174c048c2c450e951ad49bd7ad9a0
Edward Toonela <edward.toonela@icloud.com>
2022-02-23 14:38:13 -0300

    populated database with seed and created  index method listing all restaurants in restaurantscontroller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+puts 'Cleaning DB'
+
+Restaurant.destroy_all
+
+puts 'Creating restaurants'
+5.times do |t|
+  Restaurant.create!  name: "Restaurant #{t + 1}", address: "Street #{t + 1}", category: Restaurant::CATEGORY.sample
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #

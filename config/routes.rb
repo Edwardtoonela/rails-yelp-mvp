afe8efbac72fc3aedee58f7b0c6dbafc19f98154
Edward Toonela <edward.toonela@icloud.com>
2022-02-23 15:41:19 -0300

    added show basics without adding function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'restaurants', to: 'restaurants#index', as: :restaurants
-
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
